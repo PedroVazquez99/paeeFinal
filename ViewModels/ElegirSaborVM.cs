22d83a73428421883143217da60c22cd62706764
PedroVazquez99 <pvt12@gcloud.ua.es>
2025-05-05 19:32:35 +0200

    Paginar Sabores

--- ViewModels/ElegirSaborVM.cs
+++ ViewModels/ElegirSaborVM.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
 using TPVproyecto.Commands;
 using TPVproyecto.Models;
-using TPVproyecto.Services;
 
 namespace TPVproyecto.ViewModels
 {
     public class ElegirSaborVM : BaseVM
     {
-        private readonly ElegirService _dataService; // servicio obtener los tipos de la BBDD
-        public List<Sabor> Sabores { get; set; } // Obtengo los tipos de la BBDD
-        public ICommand SeleccionarCommand { get; set; }
-        private readonly ElegirVM _elegirVM;
+        public ElegirVM _mainViewModel; // Referencia al ViewModel padre
 
-        public ElegirSaborVM(ElegirVM elegirVM)
+        public ObservableCollection<Sabor> SaboresVisibles => _mainViewModel.SaboresVisibles; // Recibe los elementos paginados
+
+        public ICommand SeleccionarCommand { get; }
+
+        public ElegirSaborVM(ElegirVM mainViewModel)
         {
-            _elegirVM = elegirVM;
-            _dataService = new ElegirService();
-            Sabores = new List<Sabor>(_dataService.obtenerSabores());
-            SeleccionarCommand = new ElegirCommand(elegirVM);
+            _mainViewModel = mainViewModel;
+            SeleccionarCommand = new RelayCommand(obj => _mainViewModel.SeleccionarElemento(obj));
         }
     }
 }
